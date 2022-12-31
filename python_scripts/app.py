5cd5302572dc4fa63e6d42544d4876384449bd61
Dima Yakovlev <dima@noteunlimited.com>
2022-12-31 21:46:55 +0200

    add import

--- python_scripts/app.py
+++ python_scripts/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import subprocess
 import psutil
 import os
 app = Flask(__name__)
@@ -11,8 +12,10 @@ def home():
 
 @app.route('/start_import_script')
 def start_import_script():
+    p = subprocess.Popen("ps -aux | grep python", stdout=subprocess.PIPE, shell=True)
+    out, err = p.communicate()
     os.system("python3 -u /app/nt_import/nt_importd.py /app/nt_import/logs/run/nt_importd.pid /app/nt_import/logs/nt_importd.log restart")
-    return f'{base_html}<br><h1>Script Restart</h1>'
+    return f'{base_html}<br><h1>Script Restart</h1> <br>{out}<br>{err}'
     
 @app.route('/get_list_processes')
 def get_list_processes():
