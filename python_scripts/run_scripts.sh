455d180716acbd64f6e4666f65a87f0158806a30
Dima Yakovlev <dima@noteunlimited.com>
2023-10-30 21:55:46 +0200

    add nt_import

--- python_scripts/run_scripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Start"
-python3 /app/nt_import/nt_importd.py /app/nt_import/logs/run/nt_importd.pid /app/nt_import/logs/nt_importd.log  start
-tail -n 10 /app/nt_import/logs/nt_importd.log
